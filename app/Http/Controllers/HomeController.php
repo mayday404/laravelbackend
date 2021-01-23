630e8c07ec4805f39beb81ed42c1fa05dd089255
mayday <maykush1999@gmail.com>
2021-01-23 19:50:24 +0530

    update

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -1,11 +1,12 @@
 <?php
-$counter = 0;
+
 namespace App\Http\Controllers;
-public function index() 
-{ 
-	return view("index", [ counter => $counter ]); 
-}
+
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+use App\Complaint;
+use App\User;
 
 class HomeController extends Controller
 {
@@ -26,6 +27,36 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        if ( Auth::user()->hasRole('engineer') ) {
+            return redirect('mod/');
+        }
+
+        $counter['potholes'] = Complaint::where('type', 'pothole')->count();
+        $counter['traffic'] = Complaint::where('type', 'traffic')->count();
+        $counter['accidents'] = Complaint::where('type', 'accident')->count();
+        $counter['users'] = User::count();
+        $cords = json_encode( Complaint::with('user')->latest()->limit(10)->get() );
+
+        $title = "Pothole Detection Dashboard";
+        
+        return view('home', compact('title', 'counter', 'cords'));
+    }
+
+    public function reports($type = "")
+    {
+        if ( $type == '' ) {
+            
+            $title = "View Complaint Reports";
+            
+            return view('reports', compact('title'));
+
+        } elseif ( in_array( $type, array('traffic', 'accident', 'pothole') ) ) {
+
+            $complaints = Complaint::where( 'type', $type )->get();
+
+            $title = "This Month's " . ucfirst( $type ) . "Complaints";
+            
+            return view('reportslog', compact('complaints', 'title'));
+        }
     }
 }
