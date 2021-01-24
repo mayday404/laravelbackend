fe46ee9a149d7231337cadf98527a4ca05f04884
mayday <maykush1999@gmail.com>
2021-01-25 01:10:00 +0530

    =update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,9 +1,7 @@
-header('Content-type: application/json');
-<!doctype html>
 <html lang="{{ app()->getLocale() }}">
     <head>
         <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta http-equiv="X-UA-Compatible" content="Application/json">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>Laravel</title>
