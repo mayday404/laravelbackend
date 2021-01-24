d6e40c0d3f46eaaccc95464d80b9f694325737f5
mayday <maykush1999@gmail.com>
2021-01-24 16:17:35 +0530

    update

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -16,3 +16,4 @@ protected function jsonResponse($data, $code = 200)
     return response()->json($data, $code,
         ['Content-Type' => 'application/json;charset=UTF-8', 'Charset' => 'utf-8'], JSON_UNESCAPED_UNICODE);
 }
+?>
